632171945002a80731c1b05fe770ab24ff0e3a64
AlbertoGarrote <sugaryspider822@gmail.com>
2025-01-29 21:16:12 +0100

    carta

---
+++ My project/Assets/EscenaCarta.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscenaCarta : MonoBehaviour
+{
+    public GameObject paneltransicion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.instance.PlaySound("musicaTension", new Vector3(0, 0, 0));
+    }
+
+    public void siguienteEscena()
+    {
+        StartCoroutine("siguiente");
+    }
+
+    IEnumerator siguiente()
+    {
+        paneltransicion.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("EscenaPrincipal");
+    }
+}
