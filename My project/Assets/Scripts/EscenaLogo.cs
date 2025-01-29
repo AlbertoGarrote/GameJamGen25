405654f3114a2cd3038edd47bcbb8a16b01ed0bb
AlbertoGarrote <sugaryspider822@gmail.com>
2025-01-29 20:15:24 +0100

    logo jejejeje

---
+++ My project/Assets/Scripts/EscenaLogo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscenaLogo : MonoBehaviour
+{
+    public GameObject paneltransicion;
+    private void Start()
+    {
+        StartCoroutine("inicio");
+    }
+    IEnumerator inicio()
+    {
+        yield return new WaitForSeconds(2.5f);
+        paneltransicion.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("MenuInicial");
+    }
+}
